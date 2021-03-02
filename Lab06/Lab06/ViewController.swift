31c236b287b83b245425981d0fa7bce2d497fccd
William Wilson <s01096947@aca.tri-c.edu>
2021-03-01 22:48:27 -0500

    Lab06

---
+++ Lab06/Lab06/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Lab06
+//
+//  Created by William Wilson on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var field: UITextField!
+    @IBOutlet var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.returnKeyType = .done
+        field.autocapitalizationType = .words
+        field.autocorrectionType = .no
+        field.becomeFirstResponder()
+        field.delegate = self
+    }
+
+    @IBAction func buttonTapped() {
+        field.resignFirstResponder()
+    }
+
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if let text = textField.text {
+            print("\(text)")
+        }
+        return true
+    }
+}

f0862d48d1ea9ba4414cdfba43187b03bda01e09
William Wilson <s01096947@aca.tri-c.edu>
2021-03-29 23:56:08 -0400

    Lab10

---
+++ Lab10-NavigationController/Lab10-NavigationController/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Lab10-NavigationController
+//
+//  Created by William Wilson on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstName: UITextField?
+    @IBOutlet var middleName: UITextField?
+    @IBOutlet var lastName: UITextField?
+    @IBOutlet var addressLine1: UITextField?
+    @IBOutlet var addressLine2: UITextField?
+    @IBOutlet var city: UITextField?
+    @IBOutlet var state: UITextField?
+    @IBOutlet var zip: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let displayDataViewController = segue.destination as? DisplayDataViewController
+        else {
+            return
+        }
+        displayDataViewController.firstName = (firstName?.text)!
+        displayDataViewController.middleName = (middleName?.text)!
+        displayDataViewController.lastName = (lastName?.text)!
+        displayDataViewController.addressLine1 = (addressLine1?.text)!
+        displayDataViewController.addressLine2 = (addressLine2?.text)!
+        displayDataViewController.city = (city?.text)!
+        displayDataViewController.state = (state?.text)!
+        displayDataViewController.zip = (zip?.text)!
+    }
+}

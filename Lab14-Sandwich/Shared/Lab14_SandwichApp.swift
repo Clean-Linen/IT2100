cf6b94d64bd114cdeb1bf2e4e22c70d11c6f1a95
William Wilson <s01096947@aca.tri-c.edu>
2021-04-26 13:53:31 -0400

    Lab14-Sandwich

---
+++ Lab14-Sandwich/Shared/Lab14_SandwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab14_SandwichApp.swift
+//  Shared
+//
+//  Created by William Wilson on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Lab14_SandwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
